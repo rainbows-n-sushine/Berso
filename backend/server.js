08e88277ddff082a78fa2d2d9b0a541d78d0412c
jaddis53 <jayaddisu@gmail.com>
2024-04-07 21:09:18 +0300

    Addding all files

--- backend/server.js
+++ backend/server.js
@@ -12,47 +12,47 @@ app.use(express.json());
 
 app.post("/signup/create-user", async (req, res) => {
 
-// const {fullName,username,email,dateOfBirth,zipCode,password}=req.body;
+const {fullName,username,email,dateOfBirth,zipCode,password}=req.body;
 
-// console.log(fullName + " "+username+" "+email+" "+dateOfBirth+" "+zipCode+" "+password)
-//   // const email = "jalal@gmail.com";
-//   const isNewUser = await User.isUniqueCredentials(email,username);
-//   const user = await User({
-//     name: fullName,
-//     dob:dateOfBirth,
-//     zip_code:zipCode,
-//     email: email,
-//     password: password,
-//     username: username,
-//   });
-//   await user.save();
-// console.log()
-//   if (!isNewUser.email && !isNewUser.username) {
-//     return res.json({
-//       success: false,
-//       message: "This email and username is already in use, try sign-in",
-//     });
-//   } 
-//   else if (!isNewUser.email)
-//   {
-//     return res.json({
-//       success: false,
-//       message: "This email is already in use, try sign-in",
-//     });
+console.log(fullName + " "+username+" "+email+" "+dateOfBirth+" "+zipCode+" "+password)
+  // const email = "jalal@gmail.com";
+  const isNewUser = await User.isUniqueCredentials(email,username);
+  const user = await User({
+    name: fullName,
+    dob:dateOfBirth,
+    zip_code:zipCode,
+    email: email,
+    password: password,
+    username: username,
+  });
+  await user.save();
+console.log()
+  if (!isNewUser.email && !isNewUser.username) {
+    return res.json({
+      success: false,
+      message: "This email and username is already in use, try sign-in",
+    });
+  } 
+  else if (!isNewUser.email)
+  {
+    return res.json({
+      success: false,
+      message: "This email is already in use, try sign-in",
+    });
     
-//   }
-//   else if (!isNewUser.username)
-//   {
-//     return res.json({
-//       success: false,
-//       message: "This email is already in use, try sign-in",
-//     });
+  }
+  else if (!isNewUser.username)
+  {
+    return res.json({
+      success: false,
+      message: "This email is already in use, try sign-in",
+    });
     
-//   }
-//   else {
-//     return res.json(user);
-//     // return res.json({ success: true, message: "Check your email" });
-//   }
+  }
+  else {
+    return res.json(user);
+    // return res.json({ success: true, message: "Check your email" });
+  }
 });
 
 app.post("/auth/user-sign-in", (req, res) => {
