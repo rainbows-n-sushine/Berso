ed60aeb006e381e55e2a65e9366e44b6a2e8711c
HiwotBerhanu <88843300+HiwotBerhanu@users.noreply.github.com>
2024-04-19 17:20:32 +0300

    Update Login.js

--- frontend/assets/Pages/Login.js
+++ frontend/assets/Pages/Login.js
@@ -70,133 +70,129 @@ const Login = () => {
   return (
     <ImageBackground
       source={require("../Images/logo22.jpg")}
-        style={tw`flex-1`}
+      style={tw`flex-1`}
       resizeMode="cover"
     >
       <ScrollView
         // contentContainerStyle={tw`justify-center items-center`}
         style={{ height: windowHeight }}
       >
-          <View style={tw`flex-1 p-4 justify-center`}>
-            <View style={tw`flex-row justify-between items-center mb-4`}>
-              <AntDesign name="arrowleft" size={24} color="white" />
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate("Home");
-                }}
-              >
-                <Text style={tw`text-white`}>Skip</Text>
-              </TouchableOpacity>
-            </View>
-
-            <View style={tw`items-center mb-8`}>
-              <Image
-                source={require("../Images/logo-removebg.png")}
-                style={tw`w-32 h-32`}
-              />
-            </View>
-
-            <Text style={tw`text-lg font-bold mb-4 text-center`}>Login</Text>
-
-            <TextInput
-              style={tw`w-full h-12 border bg-white border-gray-300 rounded-full w-70 ml-10 px-4 mb-4`}
-              placeholder="enter email or username"
-              value={credential}
-              onChangeText={(text) => setCredential(text)}
-            />
-            {errors.credential && (
-              <Text style={tw`text-red-500 ml-10 mb-2`}>{errors.credential}</Text>
-            )}
-
-              <TextInput
-              style={tw`w-full h-12 border bg-white border-gray-300 rounded-full w-70 ml-10 px-4 mb-4`}
-                placeholder="Password"
-                secureTextEntry
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-              />
-              {errors.password && (
-              <Text style={tw`text-red-500 ml-10 mb-2`}>{errors.password}</Text>
-              )}
-
-              <TouchableOpacity
-              style={tw`bg-orange-500 rounded-full h-12 items-center justify-center mb-4 w-70 ml-10`}
-                onPress={handleSubmit}
-              >
-                <Text style={tw`text-white font-bold`}>Login</Text>
-              </TouchableOpacity>
-
-              <View style={tw`border-b border-gray-300 my-8`} />
-
-              <TouchableOpacity
-              style={tw`bg-orange-500 rounded-full h-12 items-center justify-center mb-4 w-60 ml-15`}
-                onPress={() => {}}
-              >
-                <View style={tw`flex-row items-center `}>
-                  <FontAwesome5
-                    name="google"
-                    size={20}
-                  color="black"
-                    style={tw`mr-2`}
-                  />
-                <Text style={tw`text-white font-bold`}>
-                    Continue with Google
-                  </Text>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={tw`bg-blue-500 rounded-full h-12 items-center justify-center mb-4 w-60 ml-15`}
-                onPress={() => {}}
-              >
-                <View style={tw`flex-row items-center`}>
-                  <FontAwesome5
-                    name="facebook"
-                    size={20}
-                    color="white"
-                    style={tw`mr-2`}
-                  />
-                  <Text style={tw`text-white font-bold`}>
-                    Continue with Facebook
-                  </Text>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={tw`bg-black rounded-full h-12 items-center justify-center mb-4 w-60 ml-15`}
-                onPress={() => {}}
-              >
-                <View style={tw`flex-row items-center justify-center`}>
-                  <FontAwesome5
-                    name="apple"
-                    size={20}
-                    color="white"
-                    style={tw`mr-2`}
-                  />
-                  <Text style={tw`text-white font-bold`}>
-                    Continue with Apple
-                  </Text>
-                </View>
-              </TouchableOpacity>
-
-            <View style={tw`flex-row justify-center mt-8`}>
-              <Text style={tw`text-sm text-white`}>
-                Don't have an account?{" "}
-              </Text>
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate("Registration");
-                }}
-              >
-                <Text style={tw`text-sm font-bold text-yellow-500`}>
-                  Sign up
-                </Text>
-              </TouchableOpacity>
-            </View>
+        <View style={tw`flex-1 p-4 justify-center`}>
+          <View style={tw`flex-row justify-between items-center mb-4`}>
+            <AntDesign name="arrowleft" size={24} color="white" />
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("Home");
+              }}
+            >
+              <Text style={tw`text-white`}>Skip</Text>
+            </TouchableOpacity>
           </View>
+
+          <View style={tw`items-center mb-8`}>
+            <Image
+              source={require("../Images/logo-removebg.png")}
+              style={tw`w-32 h-32`}
+            />
+          </View>
+
+          <Text
+            className={`text-lg font-bold mb-4 text-center`}
+            style={{ fontFamily: "berlin-sans", fontSize: 40 }}
+          >
+            Login
+          </Text>
+
+          <TextInput
+            style={tw`w-full h-12 border bg-white border-gray-300 rounded-full w-70 ml-10 px-4 mb-4`}
+            placeholder="enter email or username"
+            value={credential}
+            onChangeText={(text) => setCredential(text)}
+          />
+          {errors.credential && (
+            <Text style={tw`text-red-500 ml-10 mb-2`}>{errors.credential}</Text>
+          )}
+
+          <TextInput
+            style={tw`w-full h-12 border bg-white border-gray-300 rounded-full w-70 ml-10 px-4 mb-4`}
+            placeholder="Password"
+            secureTextEntry
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          {errors.password && (
+            <Text style={tw`text-red-500 ml-10 mb-2`}>{errors.password}</Text>
+          )}
+
+          <TouchableOpacity
+            style={tw`bg-orange-500 rounded-full h-12 items-center justify-center mb-4 w-70 ml-10`}
+            onPress={handleSubmit}
+          >
+            <Text style={tw`text-white font-bold`}>Login</Text>
+          </TouchableOpacity>
+
+          <View style={tw`border-b border-gray-300 my-8`} />
+
+          <TouchableOpacity
+            style={tw`bg-orange-500 rounded-full h-12 items-center justify-center mb-4 w-60 ml-15`}
+            onPress={() => {}}
+          >
+            <View style={tw`flex-row items-center `}>
+              <FontAwesome5
+                name="google"
+                size={20}
+                color="black"
+                style={tw`mr-2`}
+              />
+              <Text style={tw`text-white font-bold`}>Continue with Google</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={tw`bg-blue-500 rounded-full h-12 items-center justify-center mb-4 w-60 ml-15`}
+            onPress={() => {}}
+          >
+            <View style={tw`flex-row items-center`}>
+              <FontAwesome5
+                name="facebook"
+                size={20}
+                color="white"
+                style={tw`mr-2`}
+              />
+              <Text style={tw`text-white font-bold`}>
+                Continue with Facebook
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={tw`bg-black rounded-full h-12 items-center justify-center mb-4 w-60 ml-15`}
+            onPress={() => {}}
+          >
+            <View style={tw`flex-row items-center justify-center`}>
+              <FontAwesome5
+                name="apple"
+                size={20}
+                color="white"
+                style={tw`mr-2`}
+              />
+              <Text style={tw`text-white font-bold`}>Continue with Apple</Text>
+            </View>
+          </TouchableOpacity>
+
+          <View style={tw`flex-row justify-center mt-8`}>
+            <Text style={tw`text-sm text-white`}>Don't have an account? </Text>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("Registration");
+              }}
+            >
+              <Text style={tw`text-sm font-bold text-yellow-500`}>Sign up</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </ScrollView>
     </ImageBackground>
-    </SafeAreaView>
   );
 };
 
