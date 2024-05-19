94435d08ed8018a483fd88db7c75013798e5bae8
Jalal Addisu <jayaddisu@gmail.com>
2024-05-20 01:15:13 +0300

    Some clean up in business owner

---
+++ backend/models/review.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose');
+const reviewSchema=new mongoose.Schema({
+
+    name:{
+        type:String,
+        
+
+    },
+    description:{
+        type:String,
+
+    },
+    business:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Business'
+
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+
+})
+
+const Review=mongoose.model('Category',reviewSchema)
+module.exports={Review}
