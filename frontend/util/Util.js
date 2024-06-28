849ca815ab66433bf2f35135bd30586ad06fed3e
Jalal Addisu <jayaddisu@gmail.com>
2024-06-28 13:50:19 +0300

    Adding some files

--- frontend/util/Util.js
+++ frontend/util/Util.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://1192.168.231.111:8000/",
+  baseURL: "http://192.168.231.111:8000/",
   // timeout: 5000,
   // headers: {
   //   "Content-Type": "application/json",
