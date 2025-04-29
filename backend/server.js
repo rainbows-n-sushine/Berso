aa06b4e0b85694f8418329861de1167e242f4860
rainbows-n-sunshine <jalaladdisu@gmail.com>
2025-04-29 19:05:53 +0300

    Adding more porsts to the cors origin

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(
   cors({
-    origin: '*', 
+    origin: ['*',origin,admin], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
